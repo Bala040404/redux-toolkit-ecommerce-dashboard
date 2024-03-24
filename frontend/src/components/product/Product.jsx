33d1d680942358a37c08ae43ec3e634a26eff78d
Balakumar <balakumarguhesh@gmail.com>
2024-03-24 12:15:11 +0530

    added link to card

--- frontend/src/components/product/Product.jsx
+++ frontend/src/components/product/Product.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./product.css";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Product({ image, name, category, price, id }) {
   const navigate = useNavigate();
@@ -10,12 +11,14 @@ function Product({ image, name, category, price, id }) {
   }
 
   return (
-    <div className="product" onClick={redirect}>
-      <img className="productimage" src={image}></img>
-      <h2 className="productname">{name}</h2>
-      <h4 className="productcategory">{category}</h4>
-      <h3 className="productprice">{price}</h3>
-    </div>
+    <Link class="link" to={`/${id}`}>
+      <div className="product">
+        <img className="productimage" src={image}></img>
+        <h2 className="productname">{name}</h2>
+        <h4 className="productcategory">{category}</h4>
+        <h3 className="productprice">{price}</h3>
+      </div>
+    </Link>
   );
 }
 
